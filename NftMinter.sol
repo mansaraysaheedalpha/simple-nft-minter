01eaa8c0ae144b28e075e868c3a0177e0077ad40
mansaraysaheedalpha <mansaraysaheedalpha@gmail.com>
2025-09-09 22:18:22 +0000

    Completed NftMinter.sol

--- NftMinter.sol
+++ NftMinter.sol
@@ -7,6 +7,34 @@ contract NftMinter is ERC721 {
     address public owner;
     uint256 private _tokenIds;
 
+    // --- Constructor ---
+    constructor() ERC721("MyHackathonNFT", "MHN") {
+        owner = msg.sender;
+    }
+
+// --- Mint Function ---
+    function mint() public payable {
+        require(msg.value == MINT_PRICE, "Incorrect ETH amount sent.");
+        
+        _tokenIds++;
+        uint256 newItemId = _tokenIds;
+        
+        _safeMint(msg.sender, newItemId);
+    }
+
+    // --- Owner-Only Functions ---
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner.");
+        _;
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool success, ) = owner.call{value: balance}("");
+        require(success, "Transfer failed.");
+    }
+}
+
 
 
     
